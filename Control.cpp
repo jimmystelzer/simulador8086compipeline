addd4619bcf4362ae0ce724ebb2e0ffa7fe940cc
jimmy.stelzer@gmail.com <jimmy.stelzer@gmail.com>
2009-04-22 12:25:03 -0300

    Control update

--- Control.cpp
+++ Control.cpp
@@ -11,8 +11,23 @@ void Control::setOpcode(std::string opcode){
     opcode = this->hp->intToBase(this->hp->stringToInt(this->hp->baseToInt(opcode,16)),2); // hex to bin
     if((opcode.substr(0,8)).compare("1100110")==0){
         //mov reg, label
-    }else if((opcode.substr(0,4)).compare("1011")==0){
-        //mov reg, imed
+    
+    }else if((opcode.substr(0,3)).compare("1011")==0){
+        //mov reg, immed
+    }else if((opcode.substr(0,7)).compare("10001000")==0){
+	//mov reg, reg
+    }else if((opcode.substr(0,7)).compare("00000000")==0){
+	//add reg, reg
+    }else if((opcode.substr(0,7)).compare("11110100")==0){
+	//hlt
+    }else if((opcode.substr(0,7)).compare("11100000")==0){
+	//loopnz
+    }else if((opcode.substr(0,7)).compare("01110100")==0){
+	//jz label (short)
+    }else if((opcode.substr(0,7)).compare("10000000")==0){
+	//cmp reg, immed
+    }else if((opcode.substr(0,7)).compare("11101001")==0){
+	//jmp label (complemento de 2)
     }
 }
 std::string Control::getMeneumonic(){
