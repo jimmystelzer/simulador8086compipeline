f68a280ccc29af1f8a915468e884ca875aa6e2a2
unknown <Jimmy@.(none)>
2009-05-18 10:27:43 -0300

    Fix few bugs and Add project for Windows

--- MainApp.h
+++ MainApp.h
@@ -1,18 +1,12 @@
 #ifndef MAINAPP_H
 #define MAINAPP_H
 
-#ifdef __BORLANDC__
-    #pragma hdrstop
-#endif
-
-#ifndef WX_PRECOMP
-    #include <wx/app.h>
-#endif
+#include <wx/app.h>
 
 class MyApp : public wxApp
 {
-	public:
-		virtual bool OnInit();
+    public:
+        virtual bool OnInit();
 };
 
 #endif // MAINAPP_H
